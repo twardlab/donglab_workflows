e986f016b6bd9caf35630800c25875d44aeb677b
lhgarciaa <lhgarciaa@g.ucla.edu>
2023-03-16 10:43:02 -0700

    Rename temp dir cl argument, add optional output dir arg, remove unused imports

--- pipeline_building/downsample.py
+++ pipeline_building/downsample.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 # New modules to import
 import argparse
 import pathlib
@@ -12,45 +10,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from glob import glob
-from os.path import join as pathjoin
 import h5py
 import time
 
-import imp
 import sys
 sys.path.append('..')
 import donglab_workflows as dw
-imp.reload(dw)
 import PIL.Image as Image
 Image.MAX_IMAGE_PIXELS = None
 import tifffile as tf # for 16 bit tiff
-import h5py
 
 parser = argparse.ArgumentParser(description="Downsample the lightsheet")
 
 parser.add_argument("input_path",type=pathlib.Path,help="The input path, can be a directory or a filename")
 parser.add_argument("image_type",choices=["ims","tif"],help="The image type, can be either ims or tif")
 parser.add_argument("-of","--output_filename",default=None,help="The name of the generated output file")
-parser.add_argument("outdir",type=pathlib.Path,help="The temporary output directory for intermediate results")
+parser.add_argument("temp_dir",type=pathlib.Path,help="The temporary output directory for intermediate results")
 parser.add_argument("-dI",default=None,help="Deviation Index")
 parser.add_argument("-res",default=50.0,type=np.float32,help="Desired voxel size")
 parser.add_argument("-c","--channel",default=0,type=int,help="Specify channel number")
 parser.add_argument("-dss","--dataset_string",default=None)
 parser.add_argument("-cs","--chunksize",default=None,help="chunksize for looking for areas with no data and loading quickly")
 parser.add_argument("-bs","--blocksize",default=None,help="blocksizesize for looking for areas with no data and loading quickly")
+parser.add_argument("-od","--outdir",default=None,help="The output directory for downsample results")
 
 args = parser.parse_args()
 
 input_path      = args.input_path
 image_type      = args.image_type
 output_filename = args.output_filename
-outdir          = args.outdir
+temp_dir        = args.temp_dir
 dI              = args.dI
 res             = args.res
 channel         = args.channel
 dataset_string  = args.dataset_string
 chunksize       = args.chunksize
 blocksize       = args.blocksize
+outdir          = args.outdir
+
+# Assert in and out paths exists
+assert os.path.exists(input_path), f"Input path {input_path} does not exist"
+if outdir is not None:
+    assert os.path.exists(outdir), f"Output dir does not exist"
+if not os.path.isdir(temp_dir):
+    print(f'temp_dir not found, creating temp_dir {temp_dir}')
+    os.makedirs(temp_dir)
 
 # power to reduce dynamic range
 power = np.ones(1,dtype=np.float32)*0.125
@@ -66,7 +70,7 @@ class TifStack:
     def __init__(self,input_directory,pattern='*.tif'):
         self.input_directory = input_directory
         self.pattern = pattern
-        self.files = glob(pathjoin(input_directory,pattern))
+        self.files = glob(os.path.join(input_directory, pattern))
         self.files.sort()
         test = Image.open(self.files[0])
         self.nxy = test.size
@@ -83,7 +87,7 @@ class TifStack:
 # if none will load from data
 if dI is None and image_type == 'ims':
     f = h5py.File(input_path,'r')
-    dI = dw.imaris_get_pixel_size(f)    
+    dI = dw.imaris_get_pixel_size(f)
     xI = dw.imaris_get_x(f)
     f.close()
 
@@ -95,7 +99,7 @@ if dataset_string is None and image_type == 'ims':
 
 if image_type == 'tif':
     data = TifStack(input_path)
-    
+
 elif image_type == 'ims':
     data_ = h5py.File(input_path,mode='r')
     data = data_[dataset_string]
@@ -111,7 +115,7 @@ print(f'Output filename is {output_filename}')
 print(f'Resolution is {dI}')
 print(f'Desired resolution is {res}')
 print(f'Dataset string is {dataset_string}')
-print(f'Temp output dir is {outdir}')
+print(f'Temp output dir is {temp_dir}')
 
 down = np.floor(res/dI).astype(int)
 
@@ -122,7 +126,7 @@ nIreal = np.array([len(x) for x in xI])
 xId = [dw.downsample(x,[d]) for x,d in zip(xI,down)]
 dId = [x[1]-x[0] for x in xId]
 
-# Iterate over the dataset (currently not doing weights) + Save intermediate outputs 
+# Iterate over the dataset (currently not doing weights) + Save intermediate outputs
 # (each slice) in case of errors
 fig,ax = plt.subplots(2,2)
 ax = ax.ravel()
@@ -136,7 +140,7 @@ start = time.time()
 
 for i in range(nIreal[0]):
     starti = time.time()
-    outname = os.path.join(outdir,f'{i:06d}_s.npy')
+    outname = os.path.join(temp_dir,f'{i:06d}_s.npy')
 
     ##########
     # Note from Daniel on January 23, 2023
@@ -169,7 +173,7 @@ for i in range(nIreal[0]):
         #w = (s>0).astype(float)
         # this is not a good way to get weights, 
         # we need to look for a 64x64 block of all zeros
-        
+
         s_all_block = s_all.reshape(s_all.shape[0]//blocksize,blocksize,s_all.shape[1]//blocksize,blocksize)
         tmp = np.logical_not(np.all(s_all_block==0,axis=(1,3))).astype(np.uint8)
         s_all_w = np.ones_like(s_all_block)
@@ -177,7 +181,6 @@ for i in range(nIreal[0]):
         s_all_w = s_all_w.reshape(s_all.shape)
         w = s_all_w[:nIreal[1]+1,:nIreal[2]+1].astype(power.dtype)
 
-        
         sd = dw.downsample((s*w),down[1:])
         s2d = dw.downsample((s2*w),down[1:])
         wd = dw.downsample(w,down[1:])
@@ -185,11 +188,11 @@ for i in range(nIreal[0]):
         sd[np.isnan(sd)] = 0.0
         s2d /= wd
         s2d[np.isnan(s2d)] = 0.0
-    
+
         np.save(outname,sd)
         np.save(outname.replace('_s','_w'),wd)
         np.save(outname.replace('_s','_s2'),s2d)
-    
+
     ax[0].cla()
     wd0 = wd>0.0
     if np.any(wd0):
@@ -205,12 +208,12 @@ for i in range(nIreal[0]):
     working.append(sd)
     working2.append(s2d)
     workingw.append(wd)
-    
+
     if len(working) == down[0]:
         workingw_stack = np.stack(workingw)
         out = dw.downsample(np.stack(working)*workingw_stack,[down[0],1,1])
         out2 = dw.downsample(np.stack(working2)*workingw_stack,[down[0],1,1])
-        outw = dw.downsample(workingw_stack,[down[0],1,1])        
+        outw = dw.downsample(workingw_stack,[down[0],1,1])
         out /= outw
         out[np.isnan(out)] = 0.0
         out2 /= outw
@@ -239,10 +242,13 @@ for i in range(nIreal[0]):
     fig.canvas.draw()
     print(f'Finished loading slice {i} of {data.shape[0]}, time {time.time() - starti} s')
 
-output = np.concatenate(output)        
+output = np.concatenate(output)
 Id = output
 wd = np.concatenate(outputw)
 print(f'Finished downsampling, time {time.time() - start}')
+
+if outdir:
+    output_filename = os.path.join(outdir, output_filename)
 
 np.savez(output_filename,I=Id,I2=np.concatenate(output2),xI=np.array(xId,dtype='object'),w=wd) # note specify object to avoid "ragged" warning
 
